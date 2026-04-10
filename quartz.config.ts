0e68c017b49790c5e5f9b10fe120bb8f63ce820e
Jerry  Pascal <jerrypascal@JerrydeMacBook-Pro.local>
2026-04-10 18:43:03 +0800

    Quartz sync: Apr 10, 2026, 6:43 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "焦点的个人网页",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
